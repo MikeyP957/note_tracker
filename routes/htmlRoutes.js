c41461f2edd17754fe37ec008aed578872ad8f34
Mikey P <peraram2@gmail.com>
2021-03-07 22:04:40 -0800

    html routes set up

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+//Dependencies
+
+const path = require('path');
+
+//Routing
+module.exports = (app) => {
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    //if no matching route, then defaule to home
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+}
