9600d917250758c99706b17c0610a44765dd0a7f
Mikey P <peraram2@gmail.com>
2021-03-07 22:17:56 -0800

    working on api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,15 @@
+// load data
+const notesData = require('../db/db.json');
+
+// Routing
+
+module.exports = (app) => {
+
+    app.get('/api/notes', (req, res) => {
+        res.json(notesData)
+    });
+
+    app.post('/api/notes', (req, res) => {
+        notesData.push(req.body)
+    })
+}
