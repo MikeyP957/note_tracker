fb811035e2146c7fc40be4ca1e0ab3f6ab544213
Mikey P <peraram2@gmail.com>
2021-03-08 19:14:02 -0800

    working on routes. working in insomnia

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,11 +6,22 @@ const path = require('path');
 module.exports = (app) => {
 
     app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/'));
+    });
+    app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/assets/js/index.js'));
+    });
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/assets/css/style.css'));
+    });
     //if no matching route, then defaule to home
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/home.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 }
