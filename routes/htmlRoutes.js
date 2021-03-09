9265c7cffa2c5f05c9a38ef9cd6195d582fb8af5
Mikey P <peraram2@gmail.com>
2021-03-08 20:26:57 -0800

    working on express.static to access js and css

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,16 @@
 //Dependencies
-
+const express = require('express');
 const path = require('path');
 
 //Routing
 module.exports = (app) => {
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/'));
-    });
+
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-
-
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/assets/js/index.js'));
-    });
-
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/assets/css/style.css'));
-    });
+  
+    
     //if no matching route, then defaule to home
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 8080;
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
+//static files
+app.use('/assets', express.static(__dirname + '/public'))
+
 //listener
 app.listen(PORT, () => {
 console.log(`App listening on PORT: ${PORT}`);
