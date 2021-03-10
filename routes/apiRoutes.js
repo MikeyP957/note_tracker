1bd31f8c0abfaf5dfdd5059dad4d3a5bdc07c903
Mikey P <peraram2@gmail.com>
2021-03-09 22:48:38 -0800

    working on app.post function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 // load data
 const fs = require('fs');
+const uniqid = require('uniqid');
 
 // Routing
 
@@ -9,12 +10,25 @@ module.exports = (app) => {
         fs.readFile('../db/db.json', (err, data) => {
             if (err) throw err;
             
-            else res.end(data)             
+            else { 
+                let notes = JSON.parse(data);
+                res.end(notes);
+            }
         })
         
     });
 
     app.post('/api/notes', (req, res) => {
+        const newNote = req.body;
+        newNote.id = uniqid();
+        console.log(newNote, "this is newNote")
+
+        let writeNote = JSON.stringify(newNote);
+
+        fs.writeFile('../db/db.json', writeNote, (err) => {
+            if (err) throw err;
+            console.log('Data written to db.json')
+        })
       
     })
 }
